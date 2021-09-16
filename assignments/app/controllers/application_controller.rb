8b98599c36d6985a6f8696879b092fe3964025cd
Sunsun Kasajoo <st122283@ait.asia>
2021-09-16 23:02:45 +0545

    validation link added

--- assignments/app/controllers/application_controller.rb
+++ assignments/app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+    before_action :current_uri
+
+    def current_uri
+        @url = request.original_url
+    end
 end
--- assignments/config/routes.rb
+++ assignments/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'basics', to: 'basics#index'
+  # get 'basics', to: 'basics#index'
   get 'basics/news', to: 'basics#news'
   get 'basics/divide_by_zero', to: 'basics#divide_by_zero'
   root 'site#index'
