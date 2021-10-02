ceefbd888deeb7f35b8d1ac662d09407746946f4
Mufaddal Enayath <st122331@ait.asia>
2021-10-02 21:53:21 +0700

    add update cucumber tests

--- assignments/features/quotation.feature
+++ assignments/features/quotation.feature
@@ -1,10 +1,17 @@
 Feature: Quotation
 
-Scenario: Add quotations to a project
+Scenario: Manage Addition and user specific deletion of Quotations
     A user should be able to add quotations to a project.
     
     Given I want to add a quotation to the project
-    # When I hide the quotations
-    # Then I should add quotation id to cookie
-    # When I click restore
-    # Then I should see all existing quotations
+    Given There is a form to add quotation
+    When I submit the form
+    Then I should see the quote added to the quotations
+    Then I should see a link to delete quote
+    When I click the link for delete quotation
+    Then I should not see the quote
+    Then I should see a link to restore quote
+    When I click the link for restore quotation
+    Then I should see all existing quotations
+
+    
--- assignments/features/step_definitions/quotation_steps.rb
+++ assignments/features/step_definitions/quotation_steps.rb
@@ -1,19 +1,51 @@
-Given('I want to add a quotation to the project') do
+Given(/^I want to add a quotation to the project$/) do
   @quotation = FactoryBot.create :quotation
 end
 
-# When("I hide the quotations") do
-#   pending # Write code here that turns the phrase above into concrete actions
-# end
+Given(/^There is a form to add quotation$/) do
+  visit 'basics/quotations'
+  expect(page).to have_selector('form#new_quotation')
+end
 
-# Then("I should add quotation id to cookie") do
-#   pending # Write code here that turns the phrase above into concrete actions
-# end
+When(/^I submit the form$/) do
+  fill_in 'Author name', with: @quotation.author_name
+  fill_in 'New Category', with: @quotation.category
+  fill_in 'Quote', with: @quotation.quote
+  click_button 'Create'
+end
 
-# When("I click restore") do
-#   pending # Write code here that turns the phrase above into concrete actions
-# end
+Then(/^I should see the quote added to the quotations$/) do
+  expect(page).to have_content @quotation.author_name
+  expect(page).to have_content @quotation.category
+  expect(page).to have_content @quotation.quote
+end
 
-# Then("I should see all existing quotations") do
-#   pending # Write code here that turns the phrase above into concrete actions
-# end
+Then(/^I should see a link to delete quote$/) do
+  expect(page).to have_link('delete', href: '/basics/quotations?quotation_id=' + @quotation.id.to_s)
+end
+
+When(/^I click the link for delete quotation$/) do
+  find_link('delete', href: '/basics/quotations?quotation_id=' + @quotation.id.to_s).click
+end
+
+Then(/^I should not see the quote$/) do
+  p @quotation.id.to_s
+  # save_and_open_page
+  expect(page).to have_content @quotation.author_name
+  expect(page).to have_content @quotation.category
+  expect(page).to have_content @quotation.quote
+end
+
+Then(/^I should see a link to restore quote$/) do
+  expect(page).to have_link('Restore', href: '/basics/quotations?restore=')
+end
+
+When(/^I click the link for restore quotation$/) do
+  find_link('Restore', href: '/basics/quotations?restore=').click
+end
+
+Then(/^I should see all existing quotations$/) do
+  expect(page).to have_content @quotation.author_name
+  expect(page).to have_content @quotation.category
+  expect(page).to have_content @quotation.quote
+end
