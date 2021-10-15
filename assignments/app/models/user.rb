8c56c825d60d4a088f1248cc20531a3a4e14c8b0
Mufaddal Enayath <st122331@ait.asia>
2021-10-15 23:38:15 +0700

    add user table

--- assignments/app/models/user.rb
+++ assignments/app/models/user.rb
@@ -5,10 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
 
+  before_save :make_first_user_an_admin
   def self.create_from_google_data(provider_data)
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
       user.email = provider_data.info.email
       user.password = Devise.friendly_token[0, 20]
     end
+  end
+
+  def make_first_user_an_admin
+    return unless User.count.zero?
+
+    self.is_admin = true
   end
 end
--- assignments/config/routes.rb
+++ assignments/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get 'basics', to: 'basics#index'
-  root 'site#index'
+  get 'admin/users'
   get 'basics/news', to: 'basics#news'
   get 'basics/divide_by_zero', to: 'basics#divide_by_zero'
   get 'basics/divide', to: 'basics#divide'
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
   get 'basics/sql_tasks', to: 'basics#sql_tasks'
   get 'plan/index'
   get 'plan/profiles'
-  get "plan/user_management"
-
+  get 'plan/user_management'
+  root 'site#index'
 end
