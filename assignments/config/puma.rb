41c08e61f89abde416b476d90e7faab63c88ebb9
Mufaddal Enayath <st122331@ait.asia>
2021-10-16 00:07:40 +0700

    change puma file

--- assignments/config/puma.rb
+++ assignments/config/puma.rb
@@ -1,18 +1,18 @@
 # Get Rails environment
 
-rails_env = ENV.fetch("RAILS_ENV") { "development" }
+rails_env = ENV.fetch('RAILS_ENV') { 'development' }
 environment rails_env
 
 # If production mode, use two workers with preloaded application
 
-if rails_env == "production"
+if rails_env == 'production'
   ENV['https_proxy'] = 'http://192.41.170.23:3128'
   ENV['http_proxy'] = 'http://192.41.170.23:3128'
   workers 2
   preload_app!
   state_path 'tmp/sockets/puma.state'
   # Location of the control socket. Must be created in advance.
-  app_dir = File.expand_path("../..", __FILE__)
+  app_dir = File.expand_path('..', __dir__)
   bind "unix://#{app_dir}/tmp/sockets/puma.sock"
   directory '/home/deploy/web4-app/assignments/current'
 end
@@ -23,11 +23,11 @@ threads 1, 5
 
 # Run on port 3000 by default
 
-port ENV.fetch("PORT") { 3000 }
+port ENV.fetch('PORT') { 3000 }
 
 # Specify the server's PID file
 
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+pidfile ENV.fetch('PIDFILE') { 'tmp/pids/server.pid' }
 
 # Allow puma to be restarted by the "rails restart" command
 
@@ -35,4 +35,4 @@ plugin :tmp_restart
 
 # Activate the control app
 
-# activate_control_app
+activate_control_app
