0767acb6bca2539d405c34d00aea9753703cd6c7
Mufaddal Enayath <st122331@ait.asia>
2021-10-16 19:53:22 +0700

    force ssl

--- assignments/app/controllers/application_controller.rb
+++ assignments/app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
-    before_action :current_uri
+  before_action :current_uri
+  # skip_before_action :verify_authenticity_token
 
-    def current_uri
-        @url = request.original_url
-    end
+  def current_uri
+    @url = request.original_url
+  end
 end
