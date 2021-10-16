59ba33fd1bed9b6f726f7cd93c711bffa841726d
Sunsun Kasajoo <st122283@ait.asia>
2021-10-16 15:12:47 +0545

    google chart added

--- assignments/app/controllers/admin_controller.rb
+++ assignments/app/controllers/admin_controller.rb
@@ -2,6 +2,7 @@ class AdminController < ApplicationController
   def users
     ban_user(params[:user_id]) if params[:user_id]
     @users = User.all
+    @data = User.count_by_date
   end
 
   def ban_user(id); end
