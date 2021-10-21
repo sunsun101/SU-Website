3cc8770f6212a3213daa3d4df7f2638a863bfb3c
Mufaddal Enayath <st122331@ait.asia>
2021-10-21 17:08:08 +0700

    remove comments

--- assignments/app/controllers/application_controller.rb
+++ assignments/app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :current_uri
-  # skip_before_action :verify_authenticity_token
 
   def current_uri
     @url = request.original_url
