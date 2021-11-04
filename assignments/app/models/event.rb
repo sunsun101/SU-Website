3a0fe210e088e6052d072b58f381524e16592c04
Sunsun Kasajoo <st122283@ait.asia>
2021-11-04 20:19:08 +0545

    events test case added

--- assignments/app/models/event.rb
+++ assignments/app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
     has_many_attached :pictures
+    validates_presence_of :title
+    validates_presence_of :description
+    validates_presence_of :event_date
+   
 end
