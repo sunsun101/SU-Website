27559c3af4b8bcb27415622f944ebb014f87dd42
Mufaddal Enayath <st122331@ait.asia>
2021-12-02 02:34:25 +0700

    update rake test

--- assignments/db/schema.rb
+++ assignments/db/schema.rb
@@ -10,133 +10,132 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_201_173_443) do
+ActiveRecord::Schema.define(version: 2021_12_01_173443) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'active_storage_attachments', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'record_type', null: false
-    t.bigint 'record_id', null: false
-    t.bigint 'blob_id', null: false
-    t.datetime 'created_at', null: false
-    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
-    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness',
-                                                    unique: true
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table 'active_storage_blobs', force: :cascade do |t|
-    t.string 'key', null: false
-    t.string 'filename', null: false
-    t.string 'content_type'
-    t.text 'metadata'
-    t.string 'service_name', null: false
-    t.bigint 'byte_size', null: false
-    t.string 'checksum', null: false
-    t.datetime 'created_at', null: false
-    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table 'active_storage_variant_records', force: :cascade do |t|
-    t.bigint 'blob_id', null: false
-    t.string 'variation_digest', null: false
-    t.index %w[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table 'complains', force: :cascade do |t|
-    t.text 'detail'
-    t.bigint 'tag_id', null: false
-    t.string 'status'
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'subject'
-    t.string 'updatedBy'
-    t.index ['tag_id'], name: 'index_complains_on_tag_id'
-    t.index ['user_id'], name: 'index_complains_on_user_id'
+  create_table "complains", force: :cascade do |t|
+    t.text "detail"
+    t.bigint "tag_id", null: false
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "subject"
+    t.string "updatedBy"
+    t.index ["tag_id"], name: "index_complains_on_tag_id"
+    t.index ["user_id"], name: "index_complains_on_user_id"
   end
 
-  create_table 'events', force: :cascade do |t|
-    t.string 'title'
-    t.text 'description'
-    t.datetime 'event_date'
-    t.string 'created_by'
-    t.string 'updated_by'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.bigint 'tag_id'
-    t.index ['tag_id'], name: 'index_events_on_tag_id'
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "event_date"
+    t.string "created_by"
+    t.string "updated_by"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "tag_id"
+    t.index ["tag_id"], name: "index_events_on_tag_id"
   end
 
-  create_table 'quotations', force: :cascade do |t|
-    t.string 'author_name'
-    t.string 'category'
-    t.text 'quote'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "quotations", force: :cascade do |t|
+    t.string "author_name"
+    t.string "category"
+    t.text "quote"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'sessions', force: :cascade do |t|
-    t.string 'session_id', null: false
-    t.string 'data'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['session_id'], name: 'index_sessions_on_session_id', unique: true
-    t.index ['updated_at'], name: 'index_sessions_on_updated_at'
+  create_table "sessions", force: :cascade do |t|
+    t.string "session_id", null: false
+    t.string "data"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
+    t.index ["updated_at"], name: "index_sessions_on_updated_at"
   end
 
-  create_table 'su_members', force: :cascade do |t|
-    t.string 'first_name'
-    t.string 'last_name'
-    t.string 'designation'
-    t.string 'nationality'
-    t.string 'department'
-    t.string 'program'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.bigint 'tag_id'
-    t.string 'crop_x'
-    t.string 'crop_y'
-    t.string 'crop_width'
-    t.string 'crop_height'
-    t.index ['tag_id'], name: 'index_su_members_on_tag_id'
+  create_table "su_members", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "designation"
+    t.string "nationality"
+    t.string "department"
+    t.string "program"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "tag_id"
+    t.string "crop_x"
+    t.string "crop_y"
+    t.string "crop_width"
+    t.string "crop_height"
+    t.index ["tag_id"], name: "index_su_members_on_tag_id"
   end
 
-  create_table 'tags', force: :cascade do |t|
-    t.string 'name'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'email'
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'uid'
-    t.string 'provider'
-    t.boolean 'is_admin', default: false
-    t.string 'status', default: 'A'
-    t.string 'confirmation_token'
-    t.datetime 'confirmed_at'
-    t.datetime 'confirmation_sent_at'
-    t.string 'unconfirmed_email'
-    t.bigint 'tag_id'
-    t.index ['confirmation_token'], name: 'index_users_on_confirmation_token', unique: true
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
-    t.index ['tag_id'], name: 'index_users_on_tag_id'
-    t.check_constraint "(status)::text = ANY (ARRAY[('A'::character varying)::text, ('D'::character varying)::text])",
-                       name: 'status_check'
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "uid"
+    t.string "provider"
+    t.boolean "is_admin", default: false
+    t.string "status", default: "A"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.bigint "tag_id"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["tag_id"], name: "index_users_on_tag_id"
+    t.check_constraint "(status)::text = ANY (ARRAY[('A'::character varying)::text, ('D'::character varying)::text])", name: "status_check"
   end
 
-  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'complains', 'tags'
-  add_foreign_key 'complains', 'users'
-  add_foreign_key 'events', 'tags'
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "complains", "tags"
+  add_foreign_key "complains", "users"
+  add_foreign_key "events", "tags"
 end
